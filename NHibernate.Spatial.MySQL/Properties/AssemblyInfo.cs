92ba58bebc29131f391e914cfff80d93ef01da0c
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2014-03-02 22:37:33 +0100

    Fix GUIDs.

--- NHibernate.Spatial.MySQL/Properties/AssemblyInfo.cs
+++ NHibernate.Spatial.MySQL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06ed08bd-d308-4d2c-a672-f4f6f72f16f4")]
+[assembly: Guid("da48dcf3-de2e-4a74-9c57-8a77f2c025b1")]
 
 // Version information for an assembly consists of the following four values:
 //
