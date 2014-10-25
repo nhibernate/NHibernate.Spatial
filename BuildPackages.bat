e78ca1b5407c1b6d3ef9942f13b97f8e50ff77be
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2014-10-26 01:00:47 +0200

    Updated NuGet packaging script to reflect latest NHibernate version.

--- BuildPackages.bat
+++ BuildPackages.bat
@@ -1,5 +1,5 @@
 if not exist "./NuGet Packages" mkdir "./NuGet Packages"
-set version=3.3.3-pre
-call ".nuget/NuGet.exe" pack NHibernate.Spatial.MsSql2008\NHibernate.Spatial.MsSql2008.csproj -Version %version% -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
+set version=4.0.1.4000
+call ".nuget/NuGet.exe" pack NHibernate.Spatial.MsSql\NHibernate.Spatial.MsSql.csproj -Version %version% -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
 call ".nuget/NuGet.exe" pack NHibernate.Spatial.MySQL\NHibernate.Spatial.MySQL.csproj -Version %version% -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
 call ".nuget/NuGet.exe" pack NHibernate.Spatial.PostGis\NHibernate.Spatial.PostGis.csproj -Version %version% -Build -IncludeReferencedProjects -Properties Configuration=Release -OutputDirectory "./NuGet Packages"
