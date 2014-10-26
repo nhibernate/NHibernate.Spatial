b3081a449d45b260fb1fb49b7a623d327874c613
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2014-10-26 03:03:25 +0100

    Added shared AssemblyInfo file and updated assembly version and copyright notices.

--- NHibernate.Spatial/Properties/AssemblyInfo.cs
+++ NHibernate.Spatial/Properties/AssemblyInfo.cs
@@ -2,35 +2,5 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NHibernate.Spatial")]
-[assembly: AssemblyDescription("NHibernate.Spatial is a library of spatial extensions for NHibernate, and allows you to connect NHibernate to a spatially enabled database and manipulate geometries in HQL or in .NET code using NetTopologySuite, providing you with a fully integrated GIS programming experience.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NHibernate.Spatial Community")]
-[assembly: AssemblyProduct("NHibernate.Spatial")]
-[assembly: AssemblyCopyright("Copyright © 2007 Ricardo Stuven")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a2857e17-baef-4712-957c-1a89faa3985e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.3.4001")]
-[assembly: AssemblyFileVersion("3.3.3.4001")]
-[assembly: CLSCompliant(true)]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("NHibernate.Spatial is a library of spatial extensions for NHibernate, and allows you to connect NHibernate to a spatially enabled database and manipulate geometries in HQL or in .NET code using NetTopologySuite, providing you with a fully integrated GIS programming experience.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NHibernate.Spatial Community")]
+[assembly: AssemblyProduct("NHibernate.Spatial")]
+[assembly: AssemblyCopyright("Copyright © 2014 Ricardo Stuven, Surya Pratap, Andreas Ravnestad")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("4.0.1.4000")]
+[assembly: AssemblyFileVersion("4.0.1.4000")]
