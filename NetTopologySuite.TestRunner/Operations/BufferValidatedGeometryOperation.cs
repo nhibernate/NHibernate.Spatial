623c6db75988d48ac0c63255aa7c1603202ba598
Surya Pratap <pratap.surya@gmail.com>
2016-02-28 07:07:14 +0530

    Comment out unrechable code

--- NetTopologySuite.TestRunner/Operations/BufferValidatedGeometryOperation.cs
+++ NetTopologySuite.TestRunner/Operations/BufferValidatedGeometryOperation.cs
@@ -98,10 +98,11 @@ namespace Open.Topology.TestRunner.Operations
              * Return an empty GeometryCollection as the result.
              * This allows the test case to avoid specifying an exact result
              */
-            if (ReturnEmptyGeometryCollection)
-            {
-                result = result.Factory.CreateGeometryCollection(null);
-            }
+
+            //if (ReturnEmptyGeometryCollection)
+            //{
+            //    result = result.Factory.CreateGeometryCollection(null);
+            //}
             return new GeometryResult(result);
         }
 
