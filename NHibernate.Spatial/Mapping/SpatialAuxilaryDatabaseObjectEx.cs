e97b3e72081bf2e8061daa1ec8163d591db74bb5
Surya Pratap <pratap.surya@gmail.com>
2016-05-10 23:42:09 +0530

    Trying to set SRID as configuration parameter

--- NHibernate.Spatial/Mapping/SpatialAuxilaryDatabaseObjectEx.cs
+++ NHibernate.Spatial/Mapping/SpatialAuxilaryDatabaseObjectEx.cs
@@ -1,4 +1,9 @@
 ﻿using NHibernate.Cfg;
+using NHibernate.Mapping;
+using NHibernate.Spatial.Type;
+using NHibernate.Type;
+using System;
+using System.Linq;
 
 namespace NHibernate.Spatial.Mapping
 {
@@ -11,9 +16,27 @@ namespace NHibernate.Spatial.Mapping
 		/// <returns></returns>
 		public static Configuration SetSpatialAuxilaryObject(this Configuration cfg)
 		{
+			return SetSpatialAuxilaryObject(cfg, sao => { });
+		}
+
+		public static Configuration SetSpatialAuxilaryObject(this Configuration cfg, Action<SpatialAuxiliaryDatabaseObject> map)
+		{
 			var obj = new SpatialAuxiliaryDatabaseObject(cfg);
+			map(obj);
 			cfg.AddAuxiliaryDatabaseObject(obj);
 			return cfg;
+		}
+
+		public static Action<SpatialAuxiliaryDatabaseObject> GetSridMapperFor<TEntity>(this Configuration cfg, string propertyName, int srid)
+		{
+			PersistentClass targetClass = cfg.GetClassMapping(typeof(TEntity));
+			var prop = targetClass.GetProperty(propertyName);
+			var column = (Column)prop.ColumnIterator.First();
+			if (((CustomType)column.Value.Type).UserType is IGeometryUserType)
+			{
+				return x => x.SetSRID(targetClass.Table, column, srid);
+			}
+			return x => { };
 		}
 	}
 }
--- NHibernate.Spatial/Mapping/SpatialAuxiliaryDatabaseObject.cs
+++ NHibernate.Spatial/Mapping/SpatialAuxiliaryDatabaseObject.cs
@@ -23,159 +23,176 @@ using NHibernate.Spatial.Dialect;
 using NHibernate.Spatial.Type;
 using NHibernate.Type;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace NHibernate.Spatial.Mapping
 {
-    /// <summary>
-    ///
-    /// </summary>
-    ///
-    /// <example>
-    ///
-    /// Sample usage:
-    ///
-    /// Declare a geometry property specifing SRID (system reference ID):
-    ///
-    /// <code>
-    /// 	 <class name="MyGeoTableA">
-    /// 		 <property name="MyGeoColumn">
-    /// 			 <type name="NHibernate.Spatial.Type.GeometryType, NHibernate.Spatial">
-    /// 				 <param name="srid">1234</param>
-    /// 			 </type>
-    /// 		 </property>
-    /// 	 </class>
-    /// 	 <class name="MyGeoTableB">
-    /// 		 <property name="MyGeoColumn">
-    /// 			 <type name="NHibernate.Spatial.Type.GeometryType, NHibernate.Spatial">
-    /// 				 <param name="subtype">POLYGON</param>
-    /// 			 </type>
-    /// 		 </property>
-    ///  	 </class>
-    ///  	 </class>
-    /// </code>
-    ///
-    ///  * SpatialAuxiliaryDatabaseObject generates spatial schema for all configured classes
-    ///
-    /// <code>
-    /// 	<database-object>
-    /// 		<definition class="NHibernate.Spatial.Mapping.SpatialAuxiliaryDatabaseObject, NHibernate.Spatial" />
-    /// 	</database-object>
-    /// </code>
-    ///
-    ///  * For example, for PostGIS the schema generation code would look like:
-    ///
-    /// <code>
-    /// 		ALTER TABLE dbo.MyGeoTableA DROP COLUMN MyGeoColumn;
-    /// 		SELECT AddGeometryColumn('dbo', 'MyGeoTableA', 'MyGeoColumn', 1234, 'GEOMETRY');
-    ///
-    /// 		ALTER TABLE dbo.MyGeoTableB DROP COLUMN MyGeoColumn;
-    /// 		SELECT AddGeometryColumn('dbo', 'MyGeoTableB', 'MyGeoColumn', -1, 'POLYGON');
-    /// </code>
-    ///
-    ///  (Compare the values defined in the property type parameters with generated SQL code)
-    ///
-    ///  </example>
-    [Serializable]
-    public class SpatialAuxiliaryDatabaseObject : AbstractAuxiliaryDatabaseObject
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SpatialAuxiliaryDatabaseObject"/> class.
-        /// </summary>
-        /// <param name="configuration">The configuration.</param>
-        /// <remarks>
-        /// To programmatically configure the auxiliary object, we follow
-        /// the advice of passing the configuration in the constructor given at:
-        /// http://forum.hibernate.org/viewtopic.php?p=2320370#2320370
-        /// However, this approach doesn't allow to configure it fully declaratively
-        /// using the &lt;database-object&gt; element plus the option
-        /// "hibernate.hbm2ddl.auto" set to "create" or "create-drop",
-        /// Alternative solutions would be:
-        /// 1) Add a Configuration property to IMapping, or
-        /// 2) Add a Configuration parameter to IAuxiliaryDatabaseObject::SqlCreateString, or
-        /// 3) Add a SetConfiguration method to IAuxiliaryDatabaseObject.
-        /// The same should apply to IAuxiliaryDatabaseObject::SqlDropString.
-        /// </remarks>
-        public SpatialAuxiliaryDatabaseObject(Configuration configuration)
-        {
-            this.configuration = configuration;
-        }
+	/// <summary>
+	///
+	/// </summary>
+	///
+	/// <example>
+	///
+	/// Sample usage:
+	///
+	/// Declare a geometry property specifing SRID (system reference ID):
+	///
+	/// <code>
+	/// 	 <class name="MyGeoTableA">
+	/// 		 <property name="MyGeoColumn">
+	/// 			 <type name="NHibernate.Spatial.Type.GeometryType, NHibernate.Spatial">
+	/// 				 <param name="srid">1234</param>
+	/// 			 </type>
+	/// 		 </property>
+	/// 	 </class>
+	/// 	 <class name="MyGeoTableB">
+	/// 		 <property name="MyGeoColumn">
+	/// 			 <type name="NHibernate.Spatial.Type.GeometryType, NHibernate.Spatial">
+	/// 				 <param name="subtype">POLYGON</param>
+	/// 			 </type>
+	/// 		 </property>
+	///  	 </class>
+	///  	 </class>
+	/// </code>
+	///
+	///  * SpatialAuxiliaryDatabaseObject generates spatial schema for all configured classes
+	///
+	/// <code>
+	/// 	<database-object>
+	/// 		<definition class="NHibernate.Spatial.Mapping.SpatialAuxiliaryDatabaseObject, NHibernate.Spatial" />
+	/// 	</database-object>
+	/// </code>
+	///
+	///  * For example, for PostGIS the schema generation code would look like:
+	///
+	/// <code>
+	/// 		ALTER TABLE dbo.MyGeoTableA DROP COLUMN MyGeoColumn;
+	/// 		SELECT AddGeometryColumn('dbo', 'MyGeoTableA', 'MyGeoColumn', 1234, 'GEOMETRY');
+	///
+	/// 		ALTER TABLE dbo.MyGeoTableB DROP COLUMN MyGeoColumn;
+	/// 		SELECT AddGeometryColumn('dbo', 'MyGeoTableB', 'MyGeoColumn', -1, 'POLYGON');
+	/// </code>
+	///
+	///  (Compare the values defined in the property type parameters with generated SQL code)
+	///
+	///  </example>
+	[Serializable]
+	public class SpatialAuxiliaryDatabaseObject : AbstractAuxiliaryDatabaseObject
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SpatialAuxiliaryDatabaseObject"/> class.
+		/// </summary>
+		/// <param name="configuration">The configuration.</param>
+		/// <remarks>
+		/// To programmatically configure the auxiliary object, we follow
+		/// the advice of passing the configuration in the constructor given at:
+		/// http://forum.hibernate.org/viewtopic.php?p=2320370#2320370
+		/// However, this approach doesn't allow to configure it fully declaratively
+		/// using the &lt;database-object&gt; element plus the option
+		/// "hibernate.hbm2ddl.auto" set to "create" or "create-drop",
+		/// Alternative solutions would be:
+		/// 1) Add a Configuration property to IMapping, or
+		/// 2) Add a Configuration parameter to IAuxiliaryDatabaseObject::SqlCreateString, or
+		/// 3) Add a SetConfiguration method to IAuxiliaryDatabaseObject.
+		/// The same should apply to IAuxiliaryDatabaseObject::SqlDropString.
+		/// </remarks>
+		public SpatialAuxiliaryDatabaseObject(Configuration configuration)
+		{
+			this.configuration = configuration;
+			this.SridMap = new Dictionary<string, int>();
+		}
 
-        private readonly Configuration configuration;
+		private readonly Configuration configuration;
+		private Dictionary<string, int> SridMap;
 
-        //private delegate void VisitGeometryColumnDelegate(Table table, Column column);
+		//private delegate void VisitGeometryColumnDelegate(Table table, Column column);
 
-        private void VisitGeometryColumns(Action<Table, Column> visitGeometryColumn)
-        {
-            // This would be quite illegal, because "configuration" is a private
-            // field of Mapping class, the most probable implementation of IMapping.
-            //
-            //this.configuration = mapping.configuration;
+		private void VisitGeometryColumns(Action<Table, Column> visitGeometryColumn)
+		{
+			// This would be quite illegal, because "configuration" is a private
+			// field of Mapping class, the most probable implementation of IMapping.
+			//
+			//this.configuration = mapping.configuration;
 
-            foreach (PersistentClass persistentClass in this.configuration.ClassMappings)
-            {
-                Table table = persistentClass.Table;
-                foreach (Column column in table.ColumnIterator)
-                {
-                    if (column.Value.Type.ReturnedClass == typeof(IGeometry))
-                    {
-                        visitGeometryColumn(table, column);
-                    }
-                }
-            }
-        }
+			foreach (PersistentClass persistentClass in this.configuration.ClassMappings)
+			{
+				Table table = persistentClass.Table;
+				foreach (Column column in table.ColumnIterator)
+				{
+					if (column.Value.Type.ReturnedClass == typeof(IGeometry))
+					{
+						var key = table.Name + "." + column.Name;
+						if (!SridMap.ContainsKey(key))
+							SridMap[key] = 0;
 
-        /// <summary>
-        /// Creates SQL to create auxiliary database objects.
-        /// </summary>
-        /// <param name="dialect">The dialect.</param>
-        /// <param name="mapping">The mapping.</param>
-        /// <param name="defaultCatalog">The default catalog.</param>
-        /// <param name="defaultSchema">The default schema.</param>
-        /// <returns></returns>
-        public override string SqlCreateString(NHibernate.Dialect.Dialect dialect, IMapping mapping, string defaultCatalog, string defaultSchema)
-        {
-            ISpatialDialect spatialDialect = (ISpatialDialect)dialect;
-            StringBuilder builder = new StringBuilder();
+						visitGeometryColumn(table, column);
+					}
+				}
+			}
+		}
 
-            // Create general objects
-            builder.Append(spatialDialect.GetSpatialCreateString(defaultSchema));
+		/// <summary>
+		/// Creates SQL to create auxiliary database objects.
+		/// </summary>
+		/// <param name="dialect">The dialect.</param>
+		/// <param name="mapping">The mapping.</param>
+		/// <param name="defaultCatalog">The default catalog.</param>
+		/// <param name="defaultSchema">The default schema.</param>
+		/// <returns></returns>
+		public override string SqlCreateString(NHibernate.Dialect.Dialect dialect, IMapping mapping, string defaultCatalog, string defaultSchema)
+		{
+			ISpatialDialect spatialDialect = (ISpatialDialect)dialect;
+			StringBuilder builder = new StringBuilder();
 
-            // Create objects per column
-            VisitGeometryColumns((tbl, col) => ColumnVisitorSQLCreate(tbl, col, builder, defaultSchema, spatialDialect));
+			// Create general objects
+			builder.Append(spatialDialect.GetSpatialCreateString(defaultSchema));
 
-            return builder.ToString();
-        }
+			// Create objects per column
+			VisitGeometryColumns((tbl, col) => ColumnVisitorSQLCreate(tbl, col, builder, defaultSchema, spatialDialect));
 
-        private void ColumnVisitorSQLCreate(Table table, Column column, StringBuilder builder, string defaultSchema, ISpatialDialect spatialDialect)
-        {
-            IGeometryUserType geometryType = (IGeometryUserType)((CustomType)column.Value.Type).UserType;
-            int srid = geometryType.SRID;
-            string subtype = geometryType.Subtype;
-            int dimension = geometryType.Dimension;
+			return builder.ToString();
+		}
 
-            builder.Append(spatialDialect.GetSpatialCreateString(defaultSchema, table.Name, column.Name, srid, subtype, dimension, column.IsNullable));
-        }
+		private void ColumnVisitorSQLCreate(Table table, Column column, StringBuilder builder, string defaultSchema, ISpatialDialect spatialDialect)
+		{
+			IGeometryUserType geometryType = (IGeometryUserType)((CustomType)column.Value.Type).UserType;
+			int srid = geometryType.SRID;
+			string subtype = geometryType.Subtype;
+			int dimension = geometryType.Dimension;
 
-        /// <summary>
-        /// Creates SQL to drop auxiliary database objects.
-        /// </summary>
-        /// <param name="dialect">The dialect.</param>
-        /// <param name="defaultCatalog">The default catalog.</param>
-        /// <param name="defaultSchema">The default schema.</param>
-        /// <returns></returns>
-        public override string SqlDropString(NHibernate.Dialect.Dialect dialect, string defaultCatalog, string defaultSchema)
-        {
-            ISpatialDialect spatialDialect = (ISpatialDialect)dialect;
-            StringBuilder builder = new StringBuilder();
+			builder.Append(spatialDialect.GetSpatialCreateString(defaultSchema, table.Name, column.Name, srid, subtype, dimension, column.IsNullable));
+		}
 
-            // Drop objects per column
-            VisitGeometryColumns((table, column) => builder.Append(spatialDialect.GetSpatialDropString(defaultSchema, table.Name, column.Name)));
+		/// <summary>
+		/// Creates SQL to drop auxiliary database objects.
+		/// </summary>
+		/// <param name="dialect">The dialect.</param>
+		/// <param name="defaultCatalog">The default catalog.</param>
+		/// <param name="defaultSchema">The default schema.</param>
+		/// <returns></returns>
+		public override string SqlDropString(NHibernate.Dialect.Dialect dialect, string defaultCatalog, string defaultSchema)
+		{
+			ISpatialDialect spatialDialect = (ISpatialDialect)dialect;
+			StringBuilder builder = new StringBuilder();
 
-            // Drop general objects
-            builder.Append(spatialDialect.GetSpatialDropString(defaultSchema));
+			// Drop objects per column
+			VisitGeometryColumns((table, column) => builder.Append(spatialDialect.GetSpatialDropString(defaultSchema, table.Name, column.Name)));
 
-            return builder.ToString();
-        }
-    }
+			// Drop general objects
+			builder.Append(spatialDialect.GetSpatialDropString(defaultSchema));
+
+			return builder.ToString();
+		}
+
+		public void SetSRID(Table table, Column column, int srid)
+		{
+			SetSRID(table.Name, column.Name, srid);
+		}
+
+		public void SetSRID(string tableName, string columnName, int srid)
+		{
+			this.SridMap[tableName + "." + columnName] = srid;
+		}
+	}
 }
