1848c1b046c292e6795bf5b1b47896d675921a2a
Surya Pratap <pratap.surya@gmail.com>
2016-05-10 19:47:23 +0530

    Add helper for SpatialAuxilaryDatabaseObject

---
+++ NHibernate.Spatial/Mapping/SpatialAuxilaryDatabaseObjectEx.cs
@@ -0,0 +1,19 @@
+﻿using NHibernate.Cfg;
+
+namespace NHibernate.Spatial.Mapping
+{
+	public static class SpatialAuxilaryDatabaseObjectEx
+	{
+		/// <summary>
+		/// Helper for the spatial Auxilary Object
+		/// </summary>
+		/// <param name="cfg"></param>
+		/// <returns></returns>
+		public static Configuration SetSpatialAuxilaryObject(this Configuration cfg)
+		{
+			var obj = new SpatialAuxiliaryDatabaseObject(cfg);
+			cfg.AddAuxiliaryDatabaseObject(obj);
+			return cfg;
+		}
+	}
+}
