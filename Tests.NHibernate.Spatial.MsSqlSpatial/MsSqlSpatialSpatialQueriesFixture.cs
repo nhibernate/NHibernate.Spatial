d60c2c4ad802e6d1fa0fe24e6544b8bebf88e581
Peet Whittaker <peet.whittaker@jbaconsulting.com>
2017-06-20 13:28:37 +0100

    Use correct namespace for derived SpatialQueriesFixtures

--- Tests.NHibernate.Spatial.MsSqlSpatial/MsSqlSpatialSpatialQueriesFixture.cs
+++ Tests.NHibernate.Spatial.MsSqlSpatial/MsSqlSpatialSpatialQueriesFixture.cs
@@ -1,7 +1,8 @@
 using NHibernate.Cfg;
 using NUnit.Framework;
+using Tests.NHibernate.Spatial.RandomGeometries;
 
-namespace Tests.NHibernate.Spatial.RandomGeometries
+namespace Tests.NHibernate.Spatial
 {
     [TestFixture]
     public class MsSqlSpatialSpatialQueriesFixture : MsSql2008SpatialQueriesFixture
