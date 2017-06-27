83450da1229c4413a645de3bfca8821985b279ba
Peet Whittaker <peet.whittaker@jbaconsulting.com>
2017-06-27 14:44:50 +0100

    Update MsSql2012SpatialDialect to inherit from MsSql2012Dialect

--- NHibernate.Spatial.MsSql/Dialect/MsSql2012SpatialDialect.cs
+++ NHibernate.Spatial.MsSql/Dialect/MsSql2012SpatialDialect.cs
@@ -16,15 +16,11 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 using NHibernate.Dialect;
-using NHibernate.Spatial.Dialect.Function;
 using NHibernate.Spatial.Metadata;
 using NHibernate.Spatial.Type;
 using NHibernate.SqlCommand;
 using NHibernate.Type;
 using NHibernate.Util;
-using System;
-using System.Globalization;
-using System.Text;
 
 namespace NHibernate.Spatial.Dialect
 {
@@ -32,16 +28,122 @@ namespace NHibernate.Spatial.Dialect
 	/// <summary>
 	/// 
 	/// </summary>
-	public abstract class MsSql2012SpatialDialect : MsSql2008SpatialDialect, ISpatialDialect, IRegisterationAdaptor
+	public abstract class MsSql2012SpatialDialect : MsSql2012Dialect, ISpatialDialect, IRegisterationAdaptor
 	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="MsSql2008SpatialDialect"/> class.
-		/// </summary>
-		protected MsSql2012SpatialDialect(string sqlTypeName, string geometryColumnsViewName, IType geometryType)
-			: base(sqlTypeName, geometryColumnsViewName, geometryType)
+	    private readonly ISpatialDialect worker;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MsSql2012SpatialDialect"/> class.
+        /// </summary>
+        protected MsSql2012SpatialDialect(string sqlTypeName, string geometryColumnsViewName, IType geometryType)
 		{
 			SpatialDialect.LastInstantiated = this;
 			worker = new MsSql2012FunctionRegistration(this, sqlTypeName, geometryColumnsViewName, geometryType);
 		}
-	}
+
+	    #region ISpatialDialect Members
+
+	    public virtual IType GeometryType
+	    {
+	        get { return worker.GeometryType; }
+	    }
+
+	    public abstract IGeometryUserType CreateGeometryUserType();
+
+	    public SqlString GetSpatialTransformString(object geometry, int srid)
+	    {
+	        return worker.GetSpatialTransformString(geometry, srid);
+	    }
+
+	    public SqlString GetSpatialAggregateString(object geometry, SpatialAggregate aggregate)
+	    {
+	        return worker.GetSpatialAggregateString(geometry, aggregate);
+	    }
+
+	    public SqlString GetSpatialAnalysisString(object geometry, SpatialAnalysis analysis, object extraArgument)
+	    {
+	        return worker.GetSpatialAnalysisString(geometry, analysis, extraArgument);
+	    }
+
+	    public SqlString GetSpatialValidationString(object geometry, SpatialValidation validation, bool criterion)
+	    {
+	        return worker.GetSpatialValidationString(geometry, validation, criterion);
+	    }
+
+	    public SqlString GetSpatialRelateString(object geometry, object anotherGeometry, object pattern, bool isStringPattern, bool criterion)
+	    {
+	        return worker.GetSpatialRelateString(geometry, anotherGeometry, pattern, isStringPattern, criterion);
+	    }
+
+	    public SqlString GetSpatialRelationString(object geometry, SpatialRelation relation, object anotherGeometry, bool criterion)
+	    {
+	        return worker.GetSpatialRelationString(geometry, relation, anotherGeometry, criterion);
+	    }
+
+	    public SqlString GetSpatialFilterString(string tableAlias, string geometryColumnName, string primaryKeyColumnName, string tableName, Parameter parameter)
+	    {
+	        return worker.GetSpatialFilterString(tableAlias, geometryColumnName, primaryKeyColumnName, tableName, parameter);
+	    }
+
+	    public string GetSpatialCreateString(string schema)
+	    {
+	        return worker.GetSpatialCreateString(schema);
+	    }
+
+	    public string GetSpatialCreateString(string schema, string table, string column, int srid, string subtype, int dimension, bool isNullable)
+	    {
+	        return worker.GetSpatialCreateString(schema, table, column, srid, subtype, dimension, isNullable);
+	    }
+
+	    public string GetSpatialDropString(string schema)
+	    {
+	        return worker.GetSpatialDropString(schema);
+	    }
+
+	    public string GetSpatialDropString(string schema, string table, string column)
+	    {
+	        return worker.GetSpatialDropString(schema, table, column);
+	    }
+
+	    public bool SupportsSpatialMetadata(MetadataClass metadataClass)
+	    {
+	        return worker.SupportsSpatialMetadata(metadataClass);
+	    }
+
+	    // TODO: Use ISessionFactory.ConnectionProvider.Driver.MultipleQueriesSeparator
+	    public string MultipleQueriesSeparator
+	    {
+	        get { return ";"; }
+	    }
+
+	    #endregion ISpatialDialect Members
+
+	    #region IRegisterationAdaptor Members
+
+	    public new void RegisterFunction(string name, NHibernate.Dialect.Function.ISQLFunction function)
+	    {
+	        base.RegisterFunction(name, function);
+	    }
+
+	    public new string Quote(string name)
+	    {
+	        return base.Quote(name);
+	    }
+
+	    /// <summary>
+	    /// Quotes the schema.
+	    /// </summary>
+	    /// <param name="schema">The schema.</param>
+	    /// <returns></returns>
+	    public string QuoteSchema(string schema)
+	    {
+	        if (string.IsNullOrEmpty(schema))
+	        {
+	            return null;
+	        }
+	        return QuoteForSchemaName(schema) + StringHelper.Dot;
+	    }
+
+	    #endregion IRegisterationAdaptor Members
+    }
 }
