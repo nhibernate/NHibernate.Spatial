e5488ed0c4c69acd667c0a7f9aeec41652ccab34
Peet Whittaker <peet.whittaker@jbaconsulting.com>
2017-06-29 15:35:54 +0100

    Add configuration for PostGis test project

--- Tests.NHibernate.Spatial.PostGis/nhsp_test.sql
+++ Tests.NHibernate.Spatial.PostGis/nhsp_test.sql
@@ -1,7 +1,7 @@
 /* NHibernate.Spatial PostGIS Test Database Creation Script */
 
 -- Run this script as superuser using psql, i.e.:
---    psql -h localhost -p 5432 -U postgres -f path-to-this-file.sql
+--    psql -h localhost -p 5431 -U postgres -f path-to-this-file.sql
 
 CREATE ROLE nhsp_test LOGIN
   ENCRYPTED PASSWORD 'md5c7a746bb04ce57ee60350ff6a98f9ae6' -- md5('nhsp_test' + salt)
@@ -9,11 +9,10 @@ CREATE ROLE nhsp_test LOGIN
 
 CREATE DATABASE nhsp_test
   WITH OWNER = nhsp_test
+       TEMPLATE=template_postgis  -- CREATE EXTENSION is only available for PostGIS >= 2
        ENCODING = 'UTF8';
 
 \connect nhsp_test
-
-CREATE EXTENSION postgis;
 
 ALTER TABLE public.geometry_columns OWNER TO nhsp_test;
 ALTER TABLE public.spatial_ref_sys OWNER TO nhsp_test;
