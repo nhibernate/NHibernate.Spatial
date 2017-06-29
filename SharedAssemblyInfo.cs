8680adae2b2f34c6e405a29e1a0d047207241f25
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2017-06-29 09:52:16 +0200

    Update version number.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.1.1.4000")]
-[assembly: AssemblyFileVersion("4.1.1.4000")]
+[assembly: AssemblyVersion("4.1.1.4001")]
+[assembly: AssemblyFileVersion("4.1.1.4001")]
