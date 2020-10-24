357f2fd9237167dfae9130b6fb4dc5df60cc7bac
Peet Whittaker <peet.whittaker@gmail.com>
2020-10-24 15:44:39 +0100

    Add tests for 3D/4D PostGIS geometries

---
+++ Tests.NHibernate.Spatial.PostGis20/Models/PolygonM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NetTopologySuite.Geometries;
+
+namespace Tests.NHibernate.Spatial.Models
+{
+    [Serializable]
+    public class PolygonM
+    {
+        public int Id { get; set; }
+
+        public Geometry Geom { get; set; }
+    }
+}
---
+++ Tests.NHibernate.Spatial.PostGis20/Models/PolygonZM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NetTopologySuite.Geometries;
+
+namespace Tests.NHibernate.Spatial.Models
+{
+    [Serializable]
+    public class PolygonZM
+    {
+        public int Id { get; set; }
+
+        public Geometry Geom { get; set; }
+    }
+}
