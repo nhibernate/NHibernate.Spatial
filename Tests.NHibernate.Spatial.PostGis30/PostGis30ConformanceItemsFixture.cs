9d73edee7496b074f9ff8e7c06cbb6da992f4b2a
Peet Whittaker <peet.whittaker@gmail.com>
2024-01-21 17:39:47 +0000

    Add spatial dialect for PostGIS 3.0+

---
+++ Tests.NHibernate.Spatial.PostGis30/PostGis30ConformanceItemsFixture.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace Tests.NHibernate.Spatial
+{
+    [TestFixture]
+    public class PostGis30ConformanceItemsFixture : PostGis20ConformanceItemsFixture
+    {
+        protected override void Configure(Configuration configuration)
+        {
+            TestConfiguration.Configure(configuration);
+        }
+    }
+}
---
+++ Tests.NHibernate.Spatial.PostGis30/PostGis30MiscTestsFixture.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate.Cfg;
+using NUnit.Framework;
+
+namespace Tests.NHibernate.Spatial
+{
+    [TestFixture]
+    public class PostGis30MiscTestsFixture : PostGis20MiscTestsFixture
+    {
+        protected override void Configure(Configuration config)
+        {
+            TestConfiguration.Configure(config);
+        }
+    }
+}
